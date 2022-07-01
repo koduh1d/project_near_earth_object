b963e0f371f7592952b694d8801a3891245ce36f
koduh <koduh5c@gmail.com>
2022-07-01 19:53:32 +1000

    fixed docstrings

--- database.py
+++ database.py
@@ -1,17 +1,3 @@
-"""A database encapsulating collections of near-Earth objects and their close approaches.
-
-A `NEODatabase` holds an interconnected data set of NEOs and close approaches.
-It provides methods to fetch an NEO by primary designation or by name, as well
-as a method to query the set of close approaches that match a collection of
-user-specified criteria.
-
-Under normal circumstances, the main module creates one NEODatabase from the
-data on NEOs and close approaches extracted by `extract.load_neos` and
-`extract.load_approaches`.
-
-You'll edit this file in Tasks 2 and 3.
-"""
-
 import math
 
 
@@ -26,39 +12,36 @@ class NEODatabase:
     def __init__(self, neos, approaches):
         """Create a new `NEODatabase`.
 
-        As a precondition, this constructor assumes that the collections of NEOs
+        As a precondition, this constructor assumes that the collections
+        of NEOs
         and close approaches haven't yet been linked - that is, the
         `.approaches` attribute of each `NearEarthObject` resolves to an empty
         collection, and the `.neo` attribute of each `CloseApproach` is None.
 
         However, each `CloseApproach` has an attribute (`._designation`) that
         matches the `.designation` attribute of the corresponding NEO. This
-        constructor modifies the supplied NEOs and close approaches to link them
+        constructor modifies the supplied NEOs and close approaches
+        to link them
         together - after it's done, the `.approaches` attribute of each NEO has
-        a collection of that NEO's close approaches, and the `.neo` attribute of
+        a collection of that NEO's close
+        approaches, and the `.neo` attribute of
         each close approach references the appropriate NEO.
 
         :param neos: A collection of `NearEarthObject`s.
         :param approaches: A collection of `CloseApproach`es.
         """
-
-        # TODO: What additional auxiliary data structures will be useful?
         self.neos_by_designation = {}
         for n in neos:
             self.neos_by_designation[n.designation] = n
-            
         self.neos_by_name = {}
         for n in neos:
             self.neos_by_name[n.name] = n
-        
         self.approaches = {}
         for a in approaches:
             if a.designation not in self.approaches:
                 self.approaches[a.designation] = [a]
             else:
                 self.approaches[a.designation].append(a)
-
-        # TODO: Link together the NEOs and their close approaches.
         for n in self.neos_by_designation.values():
             if n.designation in self.approaches:
                 n.approaches = self.approaches[n.designation]
@@ -76,7 +59,8 @@ class NEODatabase:
         match is found.
 
         :param designation: The primary designation of the NEO to search for.
-        :return: The `NearEarthObject` with the desired primary designation, or `None`.
+        :return: The `NearEarthObject` with the desired primary
+        designation, or `None`.
         """
         # TODO: Fetch an NEO by its primary designation.
         if designation not in self.neos_by_designation:
@@ -103,21 +87,23 @@ class NEODatabase:
         return self.neos_by_name[name]
 
     def query(self, filters=()):
-        """Query close approaches to generate those that match a collection of filters.
+        """Query close approaches to generate those that match a
+        collection of filters.
 
-        This generates a stream of `CloseApproach` objects that match all of the
+        This generates a stream of `CloseApproach` objects
+        that match all of the
         provided filters.
 
         If no arguments are provided, generate all known close approaches.
 
-        The `CloseApproach` objects are generated in internal order, which isn't
+        The `CloseApproach` objects are generated in
+        internal order, which isn't
         guaranteed to be sorted meaninfully, although is often sorted by time.
 
-        :param filters: A collection of filters capturing user-specified criteria.
+        :param filters: A collection of filters capturing
+        user-specified criteria.
         :return: A stream of matching `CloseApproach` objects.
         """
-        # TODO: Generate `CloseApproach` objects that match all of the filters.
-        
         for ls in self.approaches.values():
             for a in ls:
                 invalid = False
@@ -125,29 +111,28 @@ class NEODatabase:
                     for k, v in filters.items():
                         if k == 'date' and a.time.date() != v:
                             invalid = True
-                        elif  k == 'start_date' and a.time.date() < v:
+                        if k == 'start_date' and a.time.date() < v:
                             invalid = True
-                        elif  k == 'end_date' and a.time.date() > v:
+                        if k == 'end_date' and a.time.date() > v:
                             invalid = True
-                        elif  k == 'distance_min' and a.distance < v:
+                        if k == 'distance_min' and a.distance < v:
                             invalid = True
-                        elif  k == 'distance_max' and a.distance > v:
+                        if k == 'distance_max' and a.distance > v:
                             invalid = True
-                        elif  k == 'velocity_min' and a.velocity < v:
+                        if k == 'velocity_min' and a.velocity < v:
                             invalid = True
-                        elif  k == 'velocity_max' and a.velocity > v:
+                        if k == 'velocity_max' and a.velocity > v:
                             invalid = True
-                        elif k in ('diameter_min', 'diameter_max') and math.isnan(a.neo.diameter):
+                        if k in ('diameter_min', 'diameter_max') and \
+                                math.isnan(a.neo.diameter):
                             invalid = True
-                        elif  k == 'diameter_min' and a.neo.diameter < v:
+                        if k == 'diameter_min' and a.neo.diameter < v:
                             invalid = True
-                        elif  k == 'diameter_max' and a.neo.diameter > v:
+                        if k == 'diameter_max' and a.neo.diameter > v:
                             invalid = True
-                        elif  k == 'hazardous' and a.neo.hazardous != v:
+                        if k == 'hazardous' and a.neo.hazardous != v:
                             invalid = True
-                            
                         if invalid:
                             break
-                
                 if not invalid:
                     yield a
--- write.py
+++ write.py
@@ -1,15 +1,3 @@
-"""Write a stream of close approaches to CSV or to JSON.
-
-This module exports two functions: `write_to_csv` and `write_to_json`, each of
-which accept an `results` stream of close approaches and a path to which to
-write the data.
-
-These functions are invoked by the main module with the output of the `limit`
-function and the filename supplied by the user at the command line. The file's
-extension determines which of these functions is used.
-
-You'll edit this file in Part 4.
-"""
 import csv
 import json
 
@@ -17,15 +5,25 @@ import json
 def write_to_csv(results, filename):
     """Write an iterable of `CloseApproach` objects to a CSV file.
 
-    The precise output specification is in `README.md`. Roughly, each output row
-    corresponds to the information in a single close approach from the `results`
+    The precise output specification is in `README.md`. Roughly,
+    each output row
+    corresponds to the information in a single close approach
+    from the `results`
     stream and its associated near-Earth object.
 
     :param results: An iterable of `CloseApproach` objects.
-    :param filename: A Path-like object pointing to where the data should be saved.
+    :param filename: A Path-like object pointing to where the
+    data should be saved.
     """
-    fieldnames = ('datetime_utc', 'distance_au', 'velocity_km_s', 'designation', 'name', 'diameter_km', 'potentially_hazardous')
-    # TODO: Write the results to a CSV file, following the specification in the instructions.
+    fieldnames = (
+        'datetime_utc',
+        'distance_au',
+        'velocity_km_s',
+        'designation',
+        'name',
+        'diameter_km',
+        'potentially_hazardous'
+    )
     with open(filename, 'w') as f:
         writer = csv.writer(f)
         writer.writerow(fieldnames)
@@ -34,27 +32,37 @@ def write_to_csv(results, filename):
             distance = r.distance
             velocity = r.velocity
             designation = r.designation
-            name = '' if r.neo.name in ('', None) else r.neo.name
-            diameter = '' if r.neo.diameter in ('', 'nan') else r.neo.diameter
-            hazardous = True if r.neo.hazardous in (1, 'Y', 'True', True) else False
-            
-            row = [datetime, distance, velocity, designation, name, diameter, hazardous]
+            name = '' if r.neo.name in ('', None)\
+                else r.neo.name
+            diameter = '' if r.neo.diameter in ('', 'nan')\
+                else r.neo.diameter
+            hazardous = True if r.neo.hazardous in (1, 'Y', 'True', True)\
+                else False
+            row = [
+                datetime,
+                distance,
+                velocity,
+                designation,
+                name,
+                diameter,
+                hazardous
+            ]
             writer.writerow(row)
-            
-            
+
 
 def write_to_json(results, filename):
     """Write an iterable of `CloseApproach` objects to a JSON file.
 
-    The precise output specification is in `README.md`. Roughly, the output is a
+    The precise output specification is in `README.md`.
+    Roughly, the output is a
     list containing dictionaries, each mapping `CloseApproach` attributes to
     their values and the 'neo' key mapping to a dictionary of the associated
     NEO's attributes.
 
     :param results: An iterable of `CloseApproach` objects.
-    :param filename: A Path-like object pointing to where the data should be saved.
+    :param filename: A Path-like object pointing to where the
+    data should be saved.
     """
-    # TODO: Write the results to a JSON file, following the specification in the instructions.
-    with open(results, 'w') as f:
+    with open(filename, 'w') as f:
         res = [r.serialize() for r in results]
         json.dump(res, f)
